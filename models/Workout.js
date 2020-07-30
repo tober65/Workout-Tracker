eeec92704498beba5476bc60ec71342adedfb293
tober65 <timothy.ober@gmail.com>
2020-07-30 10:01:01 -0700

    Models working

--- models/Workout.js
+++ models/Workout.js
@@ -1,7 +1,19 @@
-module.exports = {
-    
-};
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date
+    },
+    exercises: {
+      type: [],
+    }
+  });
+
+  const Workout = mongoose.model("Workout", WorkoutSchema);
+
+  module.exports = Workout;
 // {
 //     day: new Date(new Date().setDate(new Date().getDate() - 10)),
 //     exercises: [
