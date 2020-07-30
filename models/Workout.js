f73d659d02e23bae56bc42557b8c695465765d80
tober65 <timothy.ober@gmail.com>
2020-07-30 09:45:59 -0700

    Initial files

---
+++ models/Workout.js
@@ -0,0 +1,29 @@
+module.exports = {
+    
+};
+
+// {
+//     day: new Date(new Date().setDate(new Date().getDate() - 10)),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bicep Curl",
+//         duration: 20,
+//         weight: 100,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+
+// {
+//     day: new Date(new Date().setDate(new Date().getDate() - 7)),
+//     exercises: [
+//       {
+//         type: "cardio",
+//         name: "Running",
+//         duration: 25,
+//         distance: 4
+//       }
+//     ]
+//   },
