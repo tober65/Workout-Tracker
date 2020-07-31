7f3a94b5df7e0a853b1ef140bb56f82e04c30bc1
tober65 <timothy.ober@gmail.com>
2020-07-31 09:55:02 -0700

    Add middleware

--- server.js
+++ server.js
@@ -9,17 +9,24 @@
 */
 
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-
 const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-const app = express();
 
 app.get("*", (req, res) => {
     res.send("<h1>Hello</h1>");
